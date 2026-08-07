90270da8057fbb2ac9687c9ce6fb4d7565cc85db
jayrana2804 <ranaparesh1754@gmail.com>
2026-08-07 13:16:40 +0530

    Add files via upload

---
+++ student1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("**************************************************\n");
+    printf("STUDENT RECORD MANAGMENT SYSTEM\n");
+    printf("**************************************************\n\n");
+
+}
+#include<iostream>
+int main()
+{
+    cout<<"   ****";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
