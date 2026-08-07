4d9d9d5d237752d22b54c137544e954a87a2e6b1
jayrana2804 <ranaparesh1754@gmail.com>
2026-08-07 13:15:45 +0530

    Add files via upload

---
+++ new_set.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+    int main()
+    {
+                cout<<setw(50)<<"Jay Rana"<<";"<<endl;
+                cout<<setw(50)<<"Surat"<<";"<<endl;
+                cout<<setw(50)<<"Jay"<<";";
+     return 0;
+    }
