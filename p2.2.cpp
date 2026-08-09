d53c64072f2e02f2f9b0dd18b7a6fc733d2af0cb
jayrana2804 <ranaparesh1754@gmail.com>
2026-08-09 15:16:36 +0530

    Add files via upload

---
+++ p2.2.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+
+{
+    int mobile_number,semester,enrollment_number;
+    string student_name,branch;
+
+    cout<<"****************************************************"<<endl;
+    cout<<"STUDENT RECORD MANAGMENT SYSTEM"<<endl;
+    cout<<"****************************************************"<<endl;
+    cout<<"Software version"<<setw(10)<<":"<<"1.0"<<endl;
+    cout<<"Academic year"<<setw(13)<<":"<<"2026-27"<<endl;
+    cout<<"Institute Name"<<setw(12)<<":"<<"CHARUSAT"<<endl;
+    cout<<"----------------------------------------------------"<<endl;
+    cout<<"STUDENT REGISTRATION"<<endl;
+    cout<<"----------------------------------------------------"<<endl;
+
+    cout<<"Enter Enrollment Number"<<setw(9)<<":"<<""<<endl;
+    cin>>enrollment_number;
+    cout<<"Enter Student Name"<<setw(14)<<":"<<""<<endl;
+    cin>>student_name;
+    cout<<"Enter Branch"<<setw(20)<<":"<<""<<endl;
+    cin>>branch;
+    cout<<"Enter Semester"<<setw(18)<<":"<<""<<endl;
+    cin>>semester;
+    cout<<"Enter Mobile number"<<setw(13)<<":"<<""<<endl;
+    cin>>mobile_number;
+
+    cout<<"----------------------------------------------------"<<endl;
+    cout<<"STUDENT INFORMATION"<<endl;
+    cout<<"----------------------------------------------------"<<endl;
+
+    cout<<"Enrollment Number"<<setw(9)<<":"<<""<<endl;
+    cin>>enrollment_number;
+    cout<<"Student Name"<<setw(14)<<":"<<""<<endl;
+    cin>>student_name;
+    cout<<"Branch"<<setw(20)<<":"<<""<<endl;
+    cin>>branch;
+    cout<<"Semester"<<setw(18)<<":"<<""<<endl;
+    jecin>>semester;
+    cout<<"Mobile number"<<setw(13)<<":"<<""<<endl;
+    cin>>mobile_number;
+
+
+
+
+
+
+
+
+
+
+
+
+}
